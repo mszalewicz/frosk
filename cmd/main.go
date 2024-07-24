55cf43b22572d285a51102ae701b82d4806a29a9
Maciej Szalewicz <m.szalewicz@gmail.com>
2024-07-24 15:05:35 +0200

    Bootstrap structured logging and initilize connection to sqlite

---
+++ cmd/main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"log/slog"
+	"os"
+
+	"github.com/mszalewicz/frosk/backend"
+	"github.com/mszalewicz/frosk/trace"
+
+	_ "github.com/mattn/go-sqlite3"
+)
+
+func main() {
+	// TODO: set db path such that it will much OS native path scheme:
+	// 		Mac:       ~/Library/Applications Support/frosk/log
+	// 		Windows:   C:\Users\<username>\AppData\Local\frosk\log
+	// 		Linux:     /var/lib/frosk/log
+	const logPath string = "./cmd/log"
+	logFile, err := os.OpenFile(logPath, os.O_APPEND|os.O_WRONLY|os.O_CREATE, 0600)
+	if err != nil {
+		log.Fatal(fmt.Errorf("Error during opening log file: %w", err))
+	}
+	defer logFile.Close()
+	logger := slog.New(slog.NewJSONHandler(logFile, nil))
+
+	// TODO: set db path such that it will much OS native path scheme:
+	// 		Mac:       ~/Library/Applications Support/frosk/application.sqlite
+	// 		Windows:   C:\Users\<username>\AppData\Local\frosk\application.sqlite
+	// 		Linux:     /var/lib/frosk/application.sqlite
+	const applicationDB string = "./sql/application.sqlite"
+
+	backend, err := backend.Initialize(applicationDB, logger)
+
+	if err != nil {
+		logger.Error(fmt.Errorf("Error during initilization of database: %w", err).Error(), slog.String("trace", trace.CurrentWindow()))
+	}
+
+	err = backend.CreateStructure()
+
+	if err != nil {
+		logger.Error(fmt.Errorf("Error during creation of db schema: %w", err).Error(), slog.String("trace", trace.CurrentWindow()))
+	}
+}
