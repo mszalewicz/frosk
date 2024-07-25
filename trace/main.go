b011a03fdf6317723e9784a849f25b2ab8b26dc0
Maciej Szalewicz <m.szalewicz@gmail.com>
2024-07-25 09:49:28 +0200

    Implement checking current trace for structured logging

---
+++ trace/main.go
@@ -0,0 +1,14 @@
+package trace
+
+import (
+	"fmt"
+	"runtime"
+)
+
+func CurrentWindow() string {
+	pc := make([]uintptr, 15)
+	n := runtime.Callers(2, pc)
+	frames := runtime.CallersFrames(pc[:n])
+	frame, _ := frames.Next()
+	return fmt.Sprintf("%s:%d %s\n", frame.File, frame.Line, frame.Function)
+}
