270662aa479f9d6468447ad0aea64d0b789e685c
Maciej Szalewicz <m.szalewicz@gmail.com>
2024-07-27 10:47:38 +0200

    Updating sql schema to bette reflect needed crypto primitives

--- sql/schema.sql
+++ sql/schema.sql
@@ -2,15 +2,16 @@ CREATE TABLE IF NOT EXISTS passwords (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     name TEXT UNIQUE NOT NULL,
     password TEXT NOT NULL,
+    initial_vector TEXT UNIQUE NOT NULL,
     created_at TEXT NULL,
     updated_at TEXT NULL
 ) STRICT;
 
+
 CREATE TABLE IF NOT EXISTS master (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
+   	id INTEGER PRIMARY KEY AUTOINCREMENT,
     password TEXT UNIQUE NOT NULL,
-    secret_key TEXT UNIQUE NOT NULL,
-    iv TEXT UNIQUE NOT NULL,
+    salt TEXT UNIQUE NOT NULL,
     created_at TEXT NULL,
     updated_at TEXT NULL
 ) STRICT;
