27b992d7f1506e82de7d4c3207ebf9db6f71f391
Maciej Szalewicz <m.szalewicz@gmail.com>
2024-08-15 08:46:57 +0200

    Handling master password input and error that may occur

--- gui/widgets.go
+++ gui/widgets.go
@@ -12,16 +12,18 @@ import (
 )
 
 var (
-	black    = color.NRGBA{R: 0, G: 0, B: 0, A: 255}
-	green    = color.NRGBA{R: 100, G: 196, B: 166, A: 255}
-	orange   = color.NRGBA{R: 235, G: 178, B: 10, A: 100}
-	red      = color.NRGBA{R: 238, G: 78, B: 78, A: 255}
-	purple   = color.NRGBA{R: 161, G: 100, B: 196, A: 255}
 	beige    = color.NRGBA{R: 247, G: 239, B: 229, A: 255}
-	grey     = color.NRGBA{R: 240, G: 240, B: 240, A: 255}
+	black    = color.NRGBA{R: 0, G: 0, B: 0, A: 255}
 	blue     = color.NRGBA{R: 150, G: 201, B: 244, A: 255}
+	green    = color.NRGBA{R: 100, G: 196, B: 166, A: 255}
+	grey     = color.NRGBA{R: 240, G: 240, B: 240, A: 255}
+	orange   = color.NRGBA{R: 235, G: 178, B: 10, A: 100}
+	purple   = color.NRGBA{R: 161, G: 100, B: 196, A: 255}
+	red      = color.NRGBA{R: 238, G: 78, B: 78, A: 255}
 	textSize = unit.Sp(30)
 )
+
+const alphabet = "abcdefghijklmnopqrstuvwxyz" + "ABCDEFGHIJKLMNOPQRSTUVWXYZ" + "0123456789" + "!#$%&'()*+,-./:;<=>?@"
 
 func ResizeWindowInfo(window *app.Window) {
 	window.Option(app.Decorated(true))
@@ -30,7 +32,7 @@ func ResizeWindowInfo(window *app.Window) {
 	window.Option(app.Size(unit.Dp(450), unit.Dp(450)))
 }
 
-func InfoWindowWidget(gtx *layout.Context, theme *material.Theme, returnBtnWidget *widget.Clickable, list *widget.List, text *string) {
+func InfoWindowWidget(gtx *layout.Context, theme *material.Theme, returnBtnWidget *widget.Clickable, list *widget.List, text string) {
 	layout.Inset{Top: unit.Dp(20), Bottom: unit.Dp(20), Left: unit.Dp(20), Right: unit.Dp(20)}.Layout(
 		*gtx,
 		func(gtx layout.Context) layout.Dimensions {
@@ -43,7 +45,7 @@ func InfoWindowWidget(gtx *layout.Context, theme *material.Theme, returnBtnWidge
 							gtx,
 							1,
 							func(gtx layout.Context, index int) layout.Dimensions {
-								return material.Label(theme, unit.Sp(18), *text).Layout(gtx)
+								return material.Label(theme, unit.Sp(18), text).Layout(gtx)
 							},
 						)
 					},
@@ -70,11 +72,13 @@ func ResizeWindowInitialSetup(window *app.Window) {
 }
 
 type InitialSetup struct {
-	passwordInput       widget.Editor
-	passwordInputRepeat widget.Editor
+	passwordInput       *widget.Editor
+	passwordInputRepeat *widget.Editor
 
-	confirmBtnWidget widget.Clickable
-	showHidWidget    widget.Clickable
+	confirmBtnWidget *widget.Clickable
+	showHidWidget    *widget.Clickable
+
+	borderColor color.NRGBA
 }
 
 func InitialSetupWidget(gtx *layout.Context, theme *material.Theme, initialSetup *InitialSetup) {
@@ -125,10 +129,10 @@ func InitialSetupWidget(gtx *layout.Context, theme *material.Theme, initialSetup
 					return elementMargin.Layout(
 						gtx,
 						func(gtx layout.Context) layout.Dimensions {
-							inputMasterPassword := material.Editor(theme, &initialSetup.passwordInput, "Password")
+							inputMasterPassword := material.Editor(theme, initialSetup.passwordInput, "Password")
 							inputMasterPassword.TextSize = unit.Sp(20)
 							inputMasterPassword.SelectionColor = blue
-							border := widget.Border{Color: color.NRGBA{A: 0xff}, CornerRadius: unit.Dp(8), Width: unit.Dp(2)}
+							border := widget.Border{Color: initialSetup.borderColor, CornerRadius: unit.Dp(8), Width: unit.Dp(2)}
 
 							return border.Layout(gtx, func(gtx layout.Context) layout.Dimensions {
 								return layout.UniformInset(unit.Dp(10)).Layout(gtx, inputMasterPassword.Layout)
@@ -148,10 +152,10 @@ func InitialSetupWidget(gtx *layout.Context, theme *material.Theme, initialSetup
 					return elementMargin.Layout(
 						gtx,
 						func(gtx layout.Context) layout.Dimensions {
-							inputMasterPasswordRepeat := material.Editor(theme, &initialSetup.passwordInputRepeat, "Password")
+							inputMasterPasswordRepeat := material.Editor(theme, initialSetup.passwordInputRepeat, "Password")
 							inputMasterPasswordRepeat.TextSize = unit.Sp(20)
 							inputMasterPasswordRepeat.SelectionColor = blue
-							border := widget.Border{Color: color.NRGBA{A: 0xff}, CornerRadius: unit.Dp(8), Width: unit.Dp(2)}
+							border := widget.Border{Color: initialSetup.borderColor, CornerRadius: unit.Dp(8), Width: unit.Dp(2)}
 
 							return border.Layout(gtx, func(gtx layout.Context) layout.Dimensions {
 								return layout.UniformInset(unit.Dp(10)).Layout(gtx, inputMasterPasswordRepeat.Layout)
@@ -159,7 +163,6 @@ func InitialSetupWidget(gtx *layout.Context, theme *material.Theme, initialSetup
 						},
 					)
 				}),
-
 				layout.Rigid(
 					func(gtx layout.Context) layout.Dimensions {
 						return btnsMargin.Layout(
@@ -172,7 +175,7 @@ func InitialSetupWidget(gtx *layout.Context, theme *material.Theme, initialSetup
 											return elementMargin.Layout(
 												gtx,
 												func(gtx layout.Context) layout.Dimensions {
-													confirmBtn := material.Button(theme, &initialSetup.confirmBtnWidget, "CONFIRM")
+													confirmBtn := material.Button(theme, initialSetup.confirmBtnWidget, "CONFIRM")
 													confirmBtn.Background = green
 													confirmBtn.TextSize = unit.Sp(25)
 													confirmBtn.Font.Weight = font.Bold
@@ -186,7 +189,7 @@ func InitialSetupWidget(gtx *layout.Context, theme *material.Theme, initialSetup
 											return elementMargin.Layout(
 												gtx,
 												func(gtx layout.Context) layout.Dimensions {
-													confirmBtn := material.Button(theme, &initialSetup.showHidWidget, "SHOW/HIDE")
+													confirmBtn := material.Button(theme, initialSetup.showHidWidget, "SHOW/HIDE")
 													confirmBtn.Background = color.NRGBA{R: 30, G: 30, B: 30, A: 255}
 													confirmBtn.TextSize = unit.Sp(25)
 													confirmBtn.Font.Weight = font.Bold
@@ -203,4 +206,34 @@ func InitialSetupWidget(gtx *layout.Context, theme *material.Theme, initialSetup
 			)
 		},
 	)
+}
+
+func ResizeWindowLoad(window *app.Window) {
+	window.Option(app.Decorated(false))
+	window.Option(app.MinSize(unit.Dp(300), unit.Dp(300)))
+	window.Option(app.MaxSize(unit.Dp(300), unit.Dp(300)))
+	window.Option(app.Size(unit.Dp(300), unit.Dp(300)))
+}
+
+func LoadWidget(gtx *layout.Context, theme *material.Theme) {
+	layout.UniformInset(unit.Dp(20)).Layout(
+		*gtx,
+		func(gtx layout.Context) layout.Dimensions {
+			return layout.Flex{Axis: layout.Vertical, Spacing: layout.SpaceEvenly}.Layout(
+				gtx,
+				layout.Flexed(1, func(gtx layout.Context) layout.Dimensions {
+					loader := material.Loader(theme)
+					loader.Color = green
+					return loader.Layout(gtx)
+				}),
+			)
+		},
+	)
+}
+
+func ResizeWindowPasswordEntriesList(window *app.Window) {
+	window.Option(app.Decorated(true))
+	window.Option(app.MinSize(unit.Dp(300), unit.Dp(300)))
+	window.Option(app.MaxSize(unit.Dp(2000), unit.Dp(2000)))
+	window.Option(app.Size(unit.Dp(450), unit.Dp(800)))
 }
